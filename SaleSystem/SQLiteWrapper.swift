3142874b70098c06d53ebfbcd46a6da3c569a2ab
ShihFengLin <noahleft@gmail.com>
2016-09-26 22:06:46 +0800

    Add load product and load unit_price from sql

--- SaleSystem/SQLiteWrapper.swift
+++ SaleSystem/SQLiteWrapper.swift
@@ -45,8 +45,59 @@ class SQLiteWrapper {
         return retArray
     }
     
+    func loadProductList() -> [PRODUCT] {
+        var retArray: [PRODUCT] = []
+        
+        do {
+            if let rPath = path {
+                let db = try Connection(rPath)
+                
+                let users = Table("product")
+                let id = Expression<Int64>("ID")
+                let name = Expression<String?>("NAME")
+                
+                for user in try db.prepare(users) {
+                    print("id: \(user[id]), name: \(user[name])")
+                    // id: 1, name: Optional("Alice")
+                    let com = PRODUCT(aId: Int(user[id]), aName: String(describing: user[name]!))
+                    retArray.append(com)
+                }
+                // SELECT * FROM "users"
+            }
+        }
+        catch {
+            print("db load fail")
+        }
+        return retArray
+    }
     
-    
+    func loadUnitPriceList() -> [UNITPRICE] {
+        var retArray: [UNITPRICE] = []
+        
+        do {
+            if let rPath = path {
+                let db = try Connection(rPath)
+                
+                let users = Table("unitprice")
+                let id = Expression<Int64>("ID")
+                let comId = Expression<Int64>("COMP_ID")
+                let proId = Expression<Int64>("PROD_ID")
+                let price = Expression<Double>("UNIT_PRICE")
+                
+                for user in try db.prepare(users) {
+                    print("id: \(user[id]), comp_id: \(user[comId]), prod_id: \(user[proId]), unit_price: \(user[price])")
+                    // id: 1, name: Optional("Alice")
+                    let com = UNITPRICE(aId: Int(user[id]), aComId: Int(user[comId]), aProId: Int(user[proId]), aUnitPrice: Float(user[price]))
+                    retArray.append(com)
+                }
+                // SELECT * FROM "users"
+            }
+        }
+        catch {
+            print("db load fail")
+        }
+        return retArray
+    }
     
 }
 
