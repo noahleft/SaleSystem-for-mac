0732e38ec1ae0ee7f75c0acc1046a4646037e662
ShihFengLin <noahleft@gmail.com>
2016-10-04 22:47:12 +0800

    Add Form info view controller  ( show the detail info )

---
+++ SaleSystem/FormInfoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FormInfoViewController.swift
+//  SaleSystem
+//
+//  Created by 林世豐 on 04/10/2016.
+//
+//
+
+import Foundation
+import Cocoa
+
+class FormInfoViewController: NSViewController {
+    
+    var FormId : Int = -1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("print form id : \(self.FormId)")
+    }
+    
+    
+}
--- SaleSystem/FormViewController.swift
+++ SaleSystem/FormViewController.swift
@@ -25,8 +25,63 @@ class FormViewController: NSViewController {
         productList = dataManager.getProductList()
         formList    = dataManager.getFormList()
         
+        tableView.delegate   = self
+        tableView.dataSource = self
     }
     
+    
+    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "showFormInfo" {
+            let nextViewController = segue.destinationController as! FormInfoViewController
+            nextViewController.FormId = 1
+        }
+        
+    }
+    
+    
+}
+
+extension FormViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return formList.count
+    }
+    
+    
+}
+
+extension FormViewController: NSTableViewDelegate {
+    
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        var text:String = ""
+        var cellIdentifier: String = ""
+        
+        // 1
+        var textName : String = ""
+        var textId : String = ""
+        
+        textName = formList[row].Name
+        textId = "\(formList[row].Id)"
+        
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            text = textId
+            cellIdentifier = "IdCellID"
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = textName
+            cellIdentifier = "NameCellID"
+        }
+        
+        // 3
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
     
     
 }
