6767b271c4413c31da6fd14c20d8fe917b9a610f
ShihFengLin <noahleft@gmail.com>
2016-10-12 18:09:11 +0800

    initial commit for extension in swift update string extension

---
+++ SaleSystem/Extension.swift
@@ -0,0 +1,63 @@
+//
+//  Extension.swift
+//  SaleSystem
+//
+//  Created by 林世豐 on 12/10/2016.
+//
+//
+
+import Foundation
+
+
+extension String {
+    
+    
+    var lastPathComponent: String {
+        
+        get {
+            return (self as NSString).lastPathComponent
+        }
+    }
+    var pathExtension: String {
+        
+        get {
+            
+            return (self as NSString).pathExtension
+        }
+    }
+    var stringByDeletingLastPathComponent: String {
+        
+        get {
+            
+            return (self as NSString).deletingLastPathComponent
+        }
+    }
+    var stringByDeletingPathExtension: String {
+        
+        get {
+            
+            return (self as NSString).deletingPathExtension
+        }
+    }
+    var pathComponents: [String] {
+        
+        get {
+            
+            return (self as NSString).pathComponents
+        }
+    }
+    
+    func stringByAppendingPathComponent(path: String) -> String {
+        
+        let nsSt = self as NSString
+        
+        return nsSt.appendingPathComponent(path)
+    }
+    
+    func stringByAppendingPathExtension(ext: String) -> String? {
+        
+        let nsSt = self as NSString  
+        
+        return nsSt.appendingPathExtension(ext)  
+    }  
+}
